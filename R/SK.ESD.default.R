ee09c7d6cc5e93c8e7811b3825fd84ac37ddf2ea
Chakkrit T <kla@chakkrit.com>
2016-08-08 11:37:29 +0900

    refine the scripts

--- R/SK.ESD.default.R
+++ R/SK.ESD.default.R
@@ -7,6 +7,7 @@
 #' @return a SK object
 #' 
 #' @examples
+#' example
 #' sk <- SK.ESD(example)
 #' sk$original  # Original Groups
 #' sk$groups    # Corrected Groups with effect size wise
@@ -14,32 +15,44 @@
 #' 
 #' sk <- SK.ESD(melt(example), long=TRUE)
 #' 
-SK.ESD <- function(data, long=FALSE){
-    library(ScottKnott); library(reshape); library(effsize)
+
+# "SK.ESD" <-
+#     function(x, ...){
+#         UseMethod("SK.ESD")
+#     }
+
+SK.ESD.default <- function(x, long=FALSE, ...){
+    library(ScottKnott); library(reshape2); library(effsize)
     if(long){
-        tmp <- do.call(cbind, split(data, data$variable))  
+        tmp <- do.call(cbind, split(x, x$variable))  
         tmp <- tmp[,grep("value",names(tmp))]
         names(tmp) <- gsub(".value", "", names(tmp))
-        data <- tmp
+        x <- tmp
     }
-    data <- data.frame(data)
+    x <- data.frame(x)
     
     transformLog  <- function(y){ y <- log1p(y)}
-    data <- data.frame(apply(data, 2, transformLog))
+    x <- apply(x, 2, transformLog)
     
-    av <- aov(value ~ variable, data=melt(data)) 
+    av <- aov(value ~ variable, x=melt(x)) 
     sk <- SK(av, which='variable',  dispersion='s', sig.level=0.05) 
     sk$original <- sk$groups; names(sk$original) <- rownames(sk$m.inf)
     ranking <- sk$groups; names(ranking) <- rownames(sk$m.inf)
     sk$diagnosis <- NULL
     keys <- names(ranking)    
     for(k in seq(2,length(keys)) ){
-        eff <- unlist(cohen.d(data[,keys[k]], data[,keys[k-1]])[c("magnitude","estimate")])
-        sk$diagnosis <- rbind(sk$diagnosis,c(sprintf("[%d] %s (%.3f)",ranking[k-1],keys[k-1], mean(data[,keys[k-1]])),sprintf("[%d] %s (%.3f)",ranking[k],keys[k], mean(data[,keys[k]])),eff)) 
+        eff <- unlist(cohen.d(x[,keys[k]], x[,keys[k-1]])[c("magnitude","estimate")])
+        sk$diagnosis <- rbind(sk$diagnosis,c(sprintf("[%d] %s (%.3f)",ranking[k-1],keys[k-1], mean(x[,keys[k-1]])),sprintf("[%d] %s (%.3f)",ranking[k],keys[k], mean(x[,keys[k]])),eff)) 
         if(eff["magnitude"] == "negligible" && ranking[k] != ranking[k-1]){ranking[seq(k,length(keys))] = ranking[seq(k,length(keys))] - 1;}
     }
     sk$groups <- ranking
     sk$reverse <- max(ranking)-ranking+1
-    sk$diagnosis <- data.frame(sk$diagnosis)
+    sk$diagnosis <- x.frame(sk$diagnosis)
+    class(sk) <- c(class(sk),"SK.ESD")
     return(sk)
+}
+
+print.SK.ESD <- function(object, ...){
+    cat("\nGroups:\n")
+    print(object$groups)
 }
