315e0d8e4734de10ca6ac57de05648d5b6f678d6
Liping <68359092+Jasmin-321@users.noreply.github.com>
2021-09-22 21:54:39 +0200

    Fix the comparision of each pair in all treatments
    Analyze the magnitude of the difference for !!!each pair!!! for all of the treatment means of the two groups. If there is any one pair of treatment means of two groups are non-negligible, then split into two groups. Otherwise, merge into one group.

--- R/partition.R
+++ R/partition.R
@@ -41,15 +41,21 @@ PartitionParametric <- function(g,
             return(TRUE)
         }
         
-        a <- av$model[av$model[,2] == names(means[k]),1]
-        b <- av$model[av$model[,2] == names(means[g]),1]  
-
-        if (all(a==b)) {
-            return(TRUE)
+        negligible <- TRUE
+          for(i in k:(g-1)){
+            for(j in (i+1):g){
+              a <- av$model[av$model[,2] == names(means[i]),1]
+              b <- av$model[av$model[,2] == names(means[j]),1]  
+              if (!all(a==b)) {
+              magnitude <- as.character(cohen.d(a,b)$magnitude, paired=TRUE)
+              if(magnitude != "negligible"){
+                negligible <- FALSE
+              }
+            }
+          }
         }
-        
-        magnitude <- as.character(cohen.d(a,b)$magnitude, paired=TRUE)
-        return(magnitude == "negligible")
+      
+        return(negligible)
     }
     ############################################################
     
